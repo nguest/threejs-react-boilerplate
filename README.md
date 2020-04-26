b76273a5cb99c8d7ec12021fa03dde409f2e984e
Nicholas Guest <nguest@workwithopal.com>
2020-04-26 16:53:28 -0700

    first commit
    Signed-off-by: Nicholas Guest <nguest@workwithopal.com>

---
+++ README.md
@@ -0,0 +1,27 @@
+[https://github.com/nguest/fast](https://github.com/nguest/fast).
+
+## Fast
+
+Fast is a WebGL car racing game-in-progress, built using three.js and custom shaders.
+
+Tracks are created procedurally, from an array of curve points- computed from real lat/lon GPS coordinates if desired.
+
+The interface will be React.js- currently only the game engine is operational. The aim is to ultimately create a Gran Turismo or Forza-like experience - but in the browser!
+
+Visit the build-in-progress at [http://fastwebgl.herokuapp.com/](http://fastwebgl.herokuapp.com/). AWSD controls the car.
+
+The story so far:
+
+update! better trees, instanced track decals etc
+![screenshot](https://github.com/nguest/fast/blob/master/public/assets/screenshots/followcam4.jpg)
+Initial commits:
+![screenshot](https://github.com/nguest/fast/blob/master/public/assets/screenshots/followcam3.png)
+
+
+In the project directory, you run:
+
+### `yarn start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+AWSD controls the car.
---
+++ src/threeApp/components/Skybox.js
@@ -0,0 +1,55 @@
+import * as THREE from 'three';
+
+import Config from '../sceneConfig/general';
+import { promisifyLoader } from '../helpers/helpers';
+
+export class SkyBox {
+  constructor(scene) {
+    this.scene = scene;
+    this.loadAssets();
+  }
+
+  loadAssets() {
+    const path = 'https://threejsfundamentals.org/threejs/resources/images/cubemaps/computer-history-museum/';
+    const loader = new THREE.CubeTextureLoader();
+    const cubeMap = promisifyLoader(loader).load([
+      // `${path}right.jpg`,
+      // `${path}left.jpg`,
+      // `${path}top.jpg`,
+      // `${path}bottom.jpg`,
+      // `${path}front.jpg`,
+      // `${path}back.jpg`,
+      `${path}pos-x.jpg`,
+      `${path}neg-x.jpg`,
+      `${path}pos-y.jpg`,
+      `${path}neg-y.jpg`,
+      `${path}pos-z.jpg`,
+      `${path}neg-z.jpg`,
+    ]);
+
+    cubeMap.then((map) => this.createBackground(map));
+  }
+
+  createBackground(cubeMap) {
+    cubeMap.minFilter = THREE.LinearFilter;
+    this.scene.background = cubeMap;
+  }
+
+  createSkyBox(material) {
+    const size = 300;
+
+    this.skyBox = new THREE.Mesh(
+      new THREE.BoxGeometry(size, size, size),
+      material,
+    );
+
+    this.skyBox.rotation.set(0, Math.PI * 0.5, 0);
+
+    this.skyBox.matrixWorldNeedsUpdate = true;
+    this.skyBox.material.needsUpdate = true;
+    this.skyBox.position.set(...Config.skyBox.position);
+
+    this.scene.add(this.skyBox);
+    return this.skyBox;
+  }
+}
